dbe6d868551d1db317c5c7c25aa9356f6e10cc48
ravjivi <ravjivi@whs.school.nz>
2024-05-17 16:12:57 +1200

    Added click register and changes to ship images

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,38 @@
 /* 
 * PROJECT TITLE: Battleships
-* VERSION or DATE: Version 1.1, 06.05.24
+* VERSION or DATE: Version 1.2, 17.05.24
 * AUTHOR: Viraaj Ravji
 * DETAILS:
-    * This Version adds buttons for the battleships
-    * All buttons still have no function
-    * Updated the GUIWIDTH calculation to avoid stretching on wide/ultrawide monitors
-    * Added very basic textures for the battleship
+    * Changes to images and scaling
+    * Buttons register clicks but don't function
+    * Converted battleships to a object
 */
 
 /*LIBRARY*/
 import javax.swing.*;    
 import java.awt.*; 
+import java.awt.event.*;
 
-public class Main { 
+public class Main implements ActionListener { 
     //constants
     public static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
     //Takes the height of the screen and calculates size of the GUI
-    public static final int GUIHEIGHT = (int)screenSize.getHeight() / 2; 
-    public static final int GUIWIDTH = GUIHEIGHT*5/2; //width of the GUI
-    
-    public static final int GUITAB = 30; //this the tab where the title is written above the GUI screen
+    private static final int GUIHEIGHT = (int)screenSize.getHeight() / 2; 
+    private static final int GUIWIDTH = GUIHEIGHT*5/2; //width of the GUI
+    private static final int GUITAB = 30; //this the tab where the title is written above the GUI screen
+
+    private static JButton[][] DGrid = new JButton[10][10]; 
+    private static JButton[] ships = new JButton[5];
+    Object userSelection;
 
     public static void Grid() { //creates the GUI and grid
         /*grid vairables*/
         JFrame f=new JFrame("Battleships"); //creates JFrame, the GUI window
-        JButton[][] DGrid = new JButton[10][10]; 
         int tileHeight = GUIHEIGHT/10; //tile spacing vertical
         int tileWidth = GUIWIDTH/20; //tile spacing horizontal
         String[] alphabetString = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
         JLabel[] labels = new JLabel[20];
+       
         //adding buttons to GUI
         for (int y=0, yPos=0, xPos=GUITAB; y<10; y++) {
             for (int x=0; x<10; x++, xPos+=tileWidth)   {
@@ -40,6 +43,12 @@ public class Main {
                 DGrid[x][y].setBorderPainted(false);
                 DGrid[x][y].setBounds(xPos,yPos,tileWidth,tileHeight);
                 f.add(DGrid[x][y]);  
+                DGrid[x][y].addActionListener(new ActionListener(){  
+                    public void actionPerformed(ActionEvent e){  
+                        actionPerformed(e);
+                    }  
+                }); 
+
             }
             yPos+=tileHeight; 
             xPos=GUITAB;
@@ -58,35 +67,78 @@ public class Main {
                 }
             }
         }
-        
-        //Battleships right of grid
-        JButton[] ships = new JButton[5];
-       
-        ships[0]=new JButton(new ImageIcon("assets/ship_texture_h2.jpg")); 
-        ships[1]=new JButton(new ImageIcon("assets/ship_texture_h3.jpg")); 
-        ships[2]=new JButton(new ImageIcon("assets/ship_texture_h3.jpg")); 
-        ships[3]=new JButton(new ImageIcon("assets/ship_texture_h4.jpg")); 
-        ships[4]=new JButton(new ImageIcon("assets/ship_texture_h5.jpg")); 
+        Main ship0 = new Main(0, tileWidth, tileHeight, ships);
+        Main ship1 = new Main(1, tileWidth, tileHeight, ships);
+        Main ship2 = new Main(2, tileWidth, tileHeight, ships);
+        Main ship3 = new Main(3, tileWidth, tileHeight, ships);
+        Main ship4 = new Main(4, tileWidth, tileHeight, ships);
 
-        
-        
-        ships[0].setBounds(tileWidth*12,tileHeight,tileWidth,tileHeight*2);  
-        ships[1].setBounds(tileWidth*15,tileHeight,tileWidth,tileHeight*3);  
-        ships[2].setBounds(tileWidth*18,tileHeight,tileWidth,tileHeight*3);
-        ships[3].setBounds(tileWidth*27/2,tileHeight*5,tileWidth,tileHeight*4); 
-        ships[4].setBounds(tileWidth*33/2,tileHeight*5,tileWidth,tileHeight*5);
-        
         f.add(ships[0]);
         f.add(ships[1]);
         f.add(ships[2]);
         f.add(ships[3]);
         f.add(ships[4]);
+
         
+
         //GUI window properties
         f.setSize(GUIWIDTH+GUITAB+(GUIWIDTH/20), GUIHEIGHT+GUITAB+GUITAB);  
         f.setLayout(null);  
         f.setVisible(true);
     }
+
+    public Main(int n, int tileWidth, int tileHeight, JButton ships[]) {
+        switch (n) {
+            case 0:
+                ImageIcon shipImageH2 = new ImageIcon("assets/ship_texture_h2.jpg");
+                Image scaleshipImageH2 = shipImageH2.getImage().getScaledInstance(tileWidth, tileHeight*2,Image.SCALE_DEFAULT);
+                ships[n] = new JButton(new ImageIcon(scaleshipImageH2));
+                ships[n].setBounds(tileWidth*12,tileHeight,tileWidth,tileHeight*2);
+                break;
+            
+            case 1:
+                ImageIcon shipImageH3 = new ImageIcon("assets/ship_texture_h3.jpg");
+                Image scaleshipImageH3 = shipImageH3.getImage().getScaledInstance(tileWidth, tileHeight*3,Image.SCALE_DEFAULT);
+                ships[n] = new JButton(new ImageIcon(scaleshipImageH3));
+                ships[n].setBounds(tileWidth*15,tileHeight,tileWidth,tileHeight*3);
+                break;
+
+            case 2:
+                shipImageH3 = new ImageIcon("assets/ship_texture_h3.jpg");
+                scaleshipImageH3 = shipImageH3.getImage().getScaledInstance(tileWidth, tileHeight*3,Image.SCALE_DEFAULT);
+                ships[n] = new JButton(new ImageIcon(scaleshipImageH3));
+                ships[n].setBounds(tileWidth*18,tileHeight,tileWidth,tileHeight*3);
+                break;
+            case 3:
+                ImageIcon shipImageH4 = new ImageIcon("assets/ship_texture_h4.jpg");
+                Image scaleshipImageH4 = shipImageH4.getImage().getScaledInstance(tileWidth, tileHeight*4,Image.SCALE_DEFAULT);
+                ships[n] = new JButton(new ImageIcon(scaleshipImageH4));
+                ships[n].setBounds(tileWidth*27/2,tileHeight*5,tileWidth,tileHeight*4);
+                break;
+            case 4:
+                ImageIcon shipImageH5 = new ImageIcon("assets/ship_texture_h5.jpg");
+                Image scaleshipImageH5 = shipImageH5.getImage().getScaledInstance(tileWidth, tileHeight*5,Image.SCALE_DEFAULT);
+                ships[n] = new JButton(new ImageIcon(scaleshipImageH5));
+                ships[n].setBounds(tileWidth*33/2,tileHeight*5,tileWidth,tileHeight*5);
+                break;
+            
+            default:
+                System.out.println ("Error: h");
+                break;
+        }
+        ships[n].addActionListener(this);
+    }
+
+    public void actionPerformed(ActionEvent e){
+        Object src = e.getSource(); 
+        userSelection = src;
+        for (int x=0; x<5; x++) {
+            if (userSelection == ships[x]) {
+                System.out.println("User has selected Ship "+(x+1));
+            }
+        }
+        
+    }  
 
     public static void main(String[] args) {  //called when the program is run
         Grid(); //calls the grid method at the start of the program
